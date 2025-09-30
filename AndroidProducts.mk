8f117af18ea63d2c617e15b298ec6e024295449e
Ezermod <ppolaralves@gmail.com>
2025-09-30 17:49:43 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -18,3 +18,5 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/twrp_cancunf.mk
 
 COMMON_LUNCH_CHOICES := twrp_cancunf-eng
+COMMON_LUNCH_CHOICES := twrp_cancunf-user
+COMMON_LUNCH_CHOICES := twrp_cancunf-userdebug
