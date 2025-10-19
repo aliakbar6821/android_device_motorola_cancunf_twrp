831ec512ad20173ed668803b3587ceaa25d7848b
Ali Akbar <1aliakbar6821@gmail.com>
2025-10-19 15:05:34 +0500

    Update vendorboot.mk

--- vendorboot.mk
+++ vendorboot.mk
@@ -1,16 +1,21 @@
 include $(CLEAR_VARS)
 
+# Fallback if PRODUCT_OUT is not yet defined
+ifndef PRODUCT_OUT
+PRODUCT_OUT := $(OUT_DIR)/target/product/$(TARGET_DEVICE)
+endif
+
 # Output target
-INSTALLED_VENDORBOOT_TARGET := $(PRODUCT_OUT)/vendorboot.img
+INSTALLED_VENDORBOOT_TARGET := $(PRODUCT_OUT)/vendor_boot.img
 
 # Required tools and prebuilts
 MKBOOTIMG := $(HOST_OUT_EXECUTABLES)/mkbootimg
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/Image
 TARGET_PREBUILT_DTB := $(LOCAL_PATH)/prebuilt/mt6855.dtb
 
-# Build rule for vendorboot.img
+# Build rule for vendor_boot.img
 $(INSTALLED_VENDORBOOT_TARGET): $(MKBOOTIMG) $(TARGET_PREBUILT_KERNEL) $(TARGET_PREBUILT_DTB)
-	@echo "---- Building vendorboot.img ----"
+	@echo "---- Building vendor_boot.img ----"
 	mkdir -p $(PRODUCT_OUT)
 	$(MKBOOTIMG) \
 		--kernel $(TARGET_PREBUILT_KERNEL) \
@@ -21,7 +26,7 @@ $(INSTALLED_VENDORBOOT_TARGET): $(MKBOOTIMG) $(TARGET_PREBUILT_KERNEL) $(TARGET_
 		--base $(BOARD_KERNEL_BASE) \
 		--os_version $(PLATFORM_VERSION) \
 		--os_patch_level $(PLATFORM_SECURITY_PATCH) \
-		--output $(PRODUCT_OUT)/vendorboot.img
+		--output $(INSTALLED_VENDORBOOT_TARGET)
 
 .PHONY: vendorboot
 vendorboot: $(INSTALLED_VENDORBOOT_TARGET)
